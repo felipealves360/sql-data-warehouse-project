fad9ac308c1d374cd81552b01b1b1c9fba200b15
Felipe Alves <41028972+felipealves360@users.noreply.github.com>
2025-04-14 21:27:28 -0300

    Update quality_checks_silver.sql
    Updated to PostgreSQL

--- scripts/silver/quality_checks_silver.sql
+++ scripts/silver/quality_checks_silver.sql
@@ -17,209 +17,225 @@ Usage Notes:
 ===============================================================================
 */
 
--- ====================================================================
--- Checking 'silver.crm_cust_info'
--- ====================================================================
+-- ==============================================================
+-- Check if the table crm_cust_info exists and drop it if it does
+-- ==============================================================
 
--- Check for Nulls or duplicates in the primary key
+-- Check for NULLS or duplicates in primary key
 -- Expectation: no results
-SELECT cst_id,
-	   COUNT( * )
+SELECT
+    cst_id,
+    count(*)
 FROM silver.crm_cust_info
 GROUP BY cst_id
-HAVING COUNT( * )>1
-	  OR cst_id IS NULL;
+HAVING cst_id IS NULL
+    OR count(*) > 1;
 
--- Check for unwanted spaces
--- Expectation: no results
-SELECT cst_lastname
+-- Check fro unwanted spaces
+-- Expectation: no results 
+SELECT 
+    cst_firstname,
+    cst_lastname,
+    TRIM(cst_firstname) AS cst_firstname,
+    TRIM(cst_lastname) AS cst_lastname
 FROM silver.crm_cust_info
-WHERE cst_lastname!=TRIM( cst_lastname );
+WHERE cst_firstname != TRIM(cst_firstname)
+    OR cst_lastname != TRIM(cst_lastname);
 
--- Data standardization & consistency
-SELECT DISTINCT cst_marital_status
+-- Data standardization & consistensy
+-- Expectation: no results
+SELECT distinct
+    cst_marital_status
 FROM silver.crm_cust_info;
 
--- ====================================================================
--- Checking 'silver.crm_prd_info'
--- ====================================================================
-
--- Check for Nulls or duplicates in the primary key
+-- Check for NULLS or duplicates in primary key
 -- Expectation: no results
-SELECT prd_id,
-	   COUNT( * )
+SELECT
+    *
+FROM
+    (SELECT 
+        *,
+        row_number() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+    FROM silver.crm_cust_info
+    ) AS t
+WHERE flag_last > 1
+    OR cst_id IS NULL;
+
+-- Check for errors in connectivity with the erp_loc_a101 table
+-- Expectation: no results
+SELECT
+cst_id
+FROM silver.crm_cust_info
+WHERE cst_id NOT IN (SELECT 
+    sls_cust_id
+FROM silver.crm_sls_details);
+
+-- ==============================================================
+-- Check if the table crm_prd_info exists and drop it if it does
+-- ==============================================================
+-- Check for NULLS or duplicates in primary key
+-- Expectation: no results
+SELECT
+    prd_id,
+    count(*)
 FROM silver.crm_prd_info
 GROUP BY prd_id
-HAVING COUNT( * )>1
-	  OR prd_id IS NULL;
+HAVING count(*) > 1
+    OR prd_id IS NULL;
 
--- Check for unwanted spaces
+-- Check fro unwanted spaces
+-- Expectation: no results
+SELECT 
+    prd_key,
+    prd_nm
+FROM silver.crm_prd_info
+WHERE prd_key != TRIM(prd_key)
+    OR prd_nm != TRIM(prd_nm);
+
+-- Check for inconsistent costs values
+-- Expectation: no results
+SELECT distinct
+    prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost <= 0
+    OR prd_cost IS NULL
+
+-- Check for inconsistent data on prd_line
+-- Expectation: qualifyed results
+SELECT distinct
+    prd_line
+FROM silver.crm_prd_info
+
+
+-- Check for inconsistent data on prd_start_dt and prd_end_dt
 -- Expectation: no results
 SELECT
-	prd_nm
+    *
 FROM silver.crm_prd_info
-WHERE prd_nm != TRIM(prd_nm);
+WHERE prd_start_dt > prd_end_dt
 
--- Check for NULLS pr negative numbers
+-- Check for errors in connectivity with the erp_loc_a101 table
+-- Expectation: only prd_key of products that were not sold
+SELECT
+prd_key
+FROM silver.crm_prd_info
+WHERE prd_key NOT IN (SELECT 
+    sls_prd_key
+FROM silver.crm_sls_details);
+
+-- ==============================================================
+-- Check if the table crm_sls_details exists and drop it if it does
+-- ==============================================================
+
+-- Check for NULLS
 -- Expectation: no results
-SELECT prd_cost
-FROM silver.crm_prd_info
-WHERE prd_cost<0
-	  OR prd_cost IS NULL;
+SELECT
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id
+FROM silver.crm_sls_details
+WHERE sls_ord_num IS NULL
+    OR sls_prd_key IS NULL
 
--- Data standadization & consistency (check distinct values for CASE statement)
-SELECT DISTINCT prd_line
-FROM silver.crm_prd_info
-
--- Checando a conexão entre a tabela de pedidos e produtos (quais produtos não possuem pedidos)
-SELECT prd_id,
-	   cat_id,
-	   prd_key,
-	   prd_nm,
-	   prd_cost,
-	   prd_line,
-	   prd_start_dt,
-	   prd_end_dt
-FROM silver.crm_prd_info
-WHERE SUBSTRING(prd_key,7,LEN(prd_key)) NOT IN (SELECT sls_prd_key FROM bronze.crm_sls_details)
-
--- Check for invalid data orders (end date < start date)
-SELECT 
-* 
-FROM silver.crm_prd_info
-WHERE prd_end_dt < prd_start_dt
-
--- ====================================================================
--- Checking 'silver.crm_sls_details'
--- ====================================================================
-
--- Check for unwanted spaces
+-- Check for NULLSy
 -- Expectation: no results
-SELECT 
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price 
+SELECT
+    sls_order_dt
+FROM silver.crm_sls_details
+WHERE sls_order_dt IS NULL
+    OR sls_order_dt < '1900-01-01'
+    OR sls_order_dt > '2100-12-31'
+
+-- Check for inconsistent data
+-- Expectation: no results
+SELECT
+    sls_ord_num,
+    sls_prd_key
 FROM silver.crm_sls_details
 WHERE sls_ord_num != TRIM(sls_ord_num)
+    OR sls_prd_key != TRIM(sls_prd_key)
 
--- Check for invalid dates
+
+-- Check for inconsistent data on sls_sales, sls_quantity and sls_price
+-- Expectation: no results
 SELECT
-	sls_due_dt
-FROM bronze.crm_sls_details
-WHERE sls_due_dt < 0 
-	OR LEN(sls_due_dt) != 8
-	OR sls_due_dt > 20500101
-	OR sls_due_dt < 19000101
-
--- Chek for invalid date orders
-SELECT 
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price 
+    sls_sales,
+    sls_quantity,
+    sls_price
 FROM silver.crm_sls_details
-WHERE sls_order_dt > sls_ship_dt 
-	OR sls_order_dt > sls_due_dt
+WHERE sls_sales <= 0
+    OR sls_quantity <= 0
+    OR sls_price <= 0
+    OR sls_sales IS NULL
+    OR sls_quantity IS NULL
+    OR sls_price IS NULL
 
--- Check for negative numbers for sales infromation
--- Expectation: no results
-SELECT 
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price 
-FROM silver.crm_sls_details
-WHERE sls_sales != sls_quantity * sls_price
-	OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
-	OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+-- ==============================================================
+-- Checking quality of the data in the erp_cust_az12 table
+-- ==============================================================
 
--- ====================================================================
--- Checking 'silver.erp_cust_az12'
--- ====================================================================
-
--- Check for unwanted spaces
--- Expectation: no results
-SELECT 
-	*
+-- Check DISTINCT data
+SELECT distinct
+cid
 FROM silver.erp_cust_az12
-WHERE gen != TRIM(gen) 
-	OR cid != TRIM(cid)
 
--- Check for NULLS pr negative numbers
--- Expectation: no results
-SELECT 
-	*
+SELECT distinct
+gen
 FROM silver.erp_cust_az12
-WHERE cid IS NULL
-	OR bdate IS NULL
-	OR gen IS NULL
 
--- Checking conection btw erp_cust_az12 & silver.crm_cust_info after the transformation
+-- Check for TRIM errors in the cid column
 -- Expectation: no results
-SELECT 
-	cid,
-	CASE 
-    	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid))
-    	ELSE cid
-    END AS cid,
-	bdate,
-	gen
-FROM silver.erp_cust_az12
-WHERE CASE 
-    	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid))
-    	ELSE cid
-    END NOT IN (SELECT DISTINCT cst_key FROM silver.crm_cust_info)
-
--- Check for invalid dates
--- Expectation: no results 
 SELECT
-	bdate
+cid
+from silver.erp_cust_az12
+WHERE cid != TRIM(cid)
+
+-- Check for errors in connectivity with the erp_loc_a101 table
+-- Expectation: no results
+SELECT
+    cid
 FROM silver.erp_cust_az12
-WHERE bdate < '1924-01-01' -- customers with 100 years old
-	OR bdate > GETDATE( ) -- birthdate in the future
+WHERE cid NOT IN (SELECT
+    cst_key
+FROM silver.crm_cust_info)
 
--- ====================================================================
--- Checking 'silver.erp_loc_a101'
--- ====================================================================
+-- Check for DATE errors in the bdate column
+-- Expectation: NULL results
+SELECT
+    bdate
+FROM silver.erp_cust_az12
+WHERE bdate IS NULL
+    OR bdate < '1900-01-01'
+    OR bdate > '2100-12-31'
 
--- Data standardization & consistency
-SELECT DISTINCT
-	cntry
+-- ==============================================================
+-- Checking quality of the data in the erp_loc_a101 table
+-- ==============================================================
+
+-- Check for TRIM errors in the cid column
+-- Expectation: no results
+SELECT
+    cid
 FROM silver.erp_loc_a101
-ORDER BY cntry
+WHERE cid != TRIM(cid)
 
--- ====================================================================
--- Checking 'silver.erp_px_cat_g1v2'
--- ====================================================================
+SELECT
+    cntry
+FROM silver.erp_loc_a101
+WHERE cntry != TRIM(cntry)
 
--- Check unwanted spaces
-SELECT 
- id,
- cat,
- subcat,
- maintenance
-FROM bronze.erp_px_cat_g1v2
-WHERE cat != TRIM(cat)
-	OR subcat != TRIM(subcat)
-	OR maintenance != TRIM(maintenance)
 
--- Check data standardization & consitency
+-- Check for NULLS or duplicates in primary key
+-- Expectation: no results
+SELECT
+    cid,
+    cntry
+FROM silver.erp_loc_a101
+WHERE cid IS NULL
+    OR cntry IS NULL
+
+-- Chek for inconsistent data on cntry
+-- Expectation: qualified results
 SELECT DISTINCT
-	cat
-FROM bronze.erp_px_cat_g1v2
+    cntry
+FROM silver.erp_loc_a101
