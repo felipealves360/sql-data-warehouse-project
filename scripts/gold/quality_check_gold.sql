c1822e75ca73fa02d5ba0b40aee3e5d1167693b1
Felipe Alves <41028972+felipealves360@users.noreply.github.com>
2025-04-24 14:44:17 -0300

    Update quality_check_gold.sql

--- scripts/gold/quality_check_gold.sql
+++ scripts/gold/quality_check_gold.sql
@@ -1,1 +1,14 @@
+--- =================================================================================
+-- Purpose: Validates referential integrity between fact and dimension tables by identifying orphaned foreign keys
+-- in the sales fact table that don't have corresponding records in customer or product dimension tables.
+--- =================================================================================
 
+SELECT
+    *
+FROM gold.fac_sales AS f
+LEFT JOIN gold.dim_customer AS c 
+    ON f.customer_key = c.customer_key
+LEFT JOIN gold.dim_products AS p 
+    ON f.product_key = p.product_key
+WHERE f.customer_key NOT IN (SELECT customer_key FROM gold.dim_customer)
+    OR f.product_key NOT IN (SELECT product_key FROM gold.dim_products);
